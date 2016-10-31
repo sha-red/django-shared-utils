b7c33dfb2735569fb5e44f28547b6105ee65ed02
Erik Stein <erik@classlibrary.net>
2016-10-31 13:56:10 +0100

    Don't require Django to get package version.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 # Erik Stein <code@classlibrary.net>, 2007-2016
 
-from django.utils.translation import ugettext_lazy as _
-
 
 VERSION = (0, 1, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 
-SLUG_HELP = _("Kurzfassung des Namens für die Adresszeile im Browser. Vorzugsweise englisch, keine Umlaute, nur Bindestrich als Sonderzeichen.")
+try:
+    from django.utils.translation import ugettext_lazy as _
+
+    SLUG_HELP = _("Kurzfassung des Namens für die Adresszeile im Browser. Vorzugsweise englisch, keine Umlaute, nur Bindestrich als Sonderzeichen.")
+except ImportError:
+    pass
---
+++ version.py
@@ -0,0 +1,5 @@
+from __future__ import unicode_literals
+
+
+VERSION = (0, 1, 0)
+__version__ = '.'.join(map(str, VERSION))
