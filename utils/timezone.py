e5ef7bc3ff0a82de7b7e6a00ee2238b900b9a02f
Erik Stein <erik@classlibrary.net>
2016-12-02 16:31:10 +0100

    Documentation.

--- utils/timezone.py
+++ utils/timezone.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 # Erik Stein <code@classlibrary.net>, 2015
+"""
+
+Django and Timezones
+--------------------
+
+https://github.com/ixc/django-timezone/
+Helper functions for working with datetime objects without caring if they are timezone aware or not.
+
+https://github.com/michaeljohnbarr/django-timezone-utils/
+Time zone utilities for Django models.
+
+https://github.com/counsyl/django-postgres-timezones
+A sample Django application illustrating some time zone traps for the unwary.
+
+"""
 
 from django.utils import timezone
 
