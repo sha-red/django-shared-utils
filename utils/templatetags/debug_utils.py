69b328225e02180dd2781e9131934cceb7ebef8f
Erik Stein <erik@classlibrary.net>
2017-02-09 16:31:47 +0100

    Debug utility template tags.

---
+++ utils/templatetags/debug_utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+# Erik Stein <code@classlibrary.net>, 2009-2017
+
+import ipdb
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def ipdb_inspect(value):
+    ipdb.set_trace()
+    return value
+
+
+@register.simple_tag
+def ipdb_set_breakpoint():
+    ipdb.set_trace()
