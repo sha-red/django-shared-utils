d1b69388eb9febd433f26d6895fe99183d0d94c2
Erik Stein <erik@classlibrary.net>
2017-07-31 16:54:23 +0200

    functional.firstof API more flexible (accepts iterable or args).

--- utils/functional.py
+++ utils/functional.py
@@ -3,11 +3,16 @@ from __future__ import unicode_literals
 # Erik Stein <code@classlibrary.net>, 2017
 
 
-def firstof(iterable, default=None):
+def firstof(*args, default=None):
     """
     Returns the first value which is neither empty nor None.
 
     """
+    if len(args) == 1:
+        iterable = args[0]
+    else:
+        iterable = args
+
     for value in iterable:
         if value:
             return value
