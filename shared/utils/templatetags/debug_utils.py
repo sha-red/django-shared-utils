ac5032f1c2c30039b93a4782b2fec9f2bb1aeb86
Erik Stein <erik@classlibrary.net>
2018-09-21 11:02:08 +0200

    get_admin_url in debug_utils

--- shared/utils/templatetags/debug_utils.py
+++ shared/utils/templatetags/debug_utils.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-# Erik Stein <code@classlibrary.net>, 2009-2017
+
+from django import template
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
+
 
 try:
     import ipdb
-    from django import template
-
 
     register = template.Library()
-
 
     @register.filter
     def ipdb_inspect(value):
         ipdb.set_trace()
         return value
 
-
     @register.simple_tag
     def ipdb_set_breakpoint():
         ipdb.set_trace()
 
-except:
+except:  # TODO ImportError
     pass
+
+
+@register.filter
+def get_admin_url(obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model), args=(obj.id,))
+
+
