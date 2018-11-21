4360ad113a50b4ac4a19d7eaa4ebe535f6d79b0f
Erik Stein <erik@classlibrary.net>
2018-11-21 13:09:33 +0100

    USE_TRANSLATABLE_FIELDS setting.

--- shared/utils/conf.py
+++ shared/utils/conf.py
@@ -1,3 +1,6 @@
 from django.conf import settings
 
-USE_TRANSLATABLE_FIELDS = getattr(settings, 'USE_TRANSLATABLE_FIELDS', False)
+USE_TRANSLATABLE_FIELDS = (
+    getattr(settings, 'CONTENT_PLUGINS_USE_TRANSLATABLE_FIELDS', False) or
+    getattr(settings, 'USE_TRANSLATABLE_FIELDS', False)
+)
